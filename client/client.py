c25ec82655b6145981802e33fcee56275f4ab214
David Morais <david_morais35@live.com.pt>
2020-12-30 12:03:15 +0000

    Improve aspects of confidentiality and integrity

--- client/client.py
+++ client/client.py
@@ -2,6 +2,7 @@ import requests
 import logging
 import binascii
 import json
+import base64
 import os
 import subprocess
 import time
@@ -12,6 +13,8 @@ from cryptography.hazmat.primitives.serialization import Encoding, ParameterForm
 from cryptography.hazmat.primitives.asymmetric import dh
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives import padding
+
 
 
 logger = logging.getLogger('root')
@@ -21,46 +24,122 @@ logger.setLevel(logging.INFO)
 
 SERVER_URL = 'http://127.0.0.1:8080'
 
+derived_key = None
+cipher = None
+mode = None
+digest = None
+parameters = None
+private_key = None
+
+def send(method, endpoint, message=None):
+    if message is None and method == 'GET':
+        return requests.get(url= f'{SERVER_URL}/{endpoint}')  
+
+    if method == 'GET':
+        return requests.get(url= f'{SERVER_URL}/{endpoint}', params= message)  
+    
+    if method == 'POST':
+        return requests.post(url= f'{SERVER_URL}/{endpoint}', data= message)  
+
+def receive(requests):
+    message = requests.json()
+
+    # if its a secure message we should authenticate it via the mac
+    if message.get('type') == 'MAC_MSG':
+        data = message.get('data').encode('latin')
+        if authenticate_mac( binascii.a2b_base64(message.get('mac') ), data):
+            message = data
+        else:
+            print(f'MAC authentication failed -- message compromised')
+        message = json.loads(message)
+
+    if message.get('type') == 'SECURE_MSG':
+        data = base64.b64decode(message.get('data'))
+        iv = base64.b64decode(message.get('iv'))
+
+        message = decrypt(data, iv)
+        message = json.loads(message)
+
+    return message
 
 
-def main():
-    derived_key = None
-    cipher = None
+def authenticate_mac(mac, message):
+    #mac = binascii.a2b_base64(mac)
 
-    def send(method, endpoint, message):
-        if derived_key and cipher:
-            print("secure connection");
+    global cipher
+    global digest
 
-        if method == 'GET':
-            return requests.get(url= f'{SERVER_URL}/{endpoint}', params= message)  
-        elif method == 'POST':
-            return requests.post(url= f'{SERVER_URL}/{endpoint}', data= message)  
+    # Slice key based on algorithm
+    if cipher == "ChaCha20":
+        key = derived_key[:32]
+    elif cipher == "AES":
+        key = derived_key[:16]
+    elif cipher == "3DES":
+        key = derived_key[:8]
 
+    # select the hash algoritm defined in the negotiations
+    if digest == 'SHA-256':
+        algorithm = hashes.SHA256()
+    elif digest == 'SHA-384':
+        algorithm = hashes.SHA384()
+    elif digest == 'SHA-512':
+        algorithm = hashes.SHA512()
 
-    print("|--------------------------------------|")
-    print("|         SECURE MEDIA CLIENT          |")
-    print("|--------------------------------------|\n")
+    _digest = hmac.HMAC(key, algorithm, backend=default_backend())
+    _digest.update(message)
 
-    # Get a list of media files
-    print("Contacting Server")
-    parameters = None
-    private_key = None
+    try:
+        _digest.verify(mac)
+        return True
+    except:
+        return False
 
-    # TODO: Secure the session
-    req = requests.get(f'{SERVER_URL}/api/protocols')
-    if req.status_code == 200:
-        prots = req.json()
+def decrypt(cryptogram, iv):
 
-    message = {
-        'type': 'NEGOTIATE',
-        'mode': ["CBC", "GCM", "ECB"], 
-        'cipher': ["ChaCha20", "AES", "3DES"], 
-        'digest': ["SHA-256", "SHA-384", "SHA-512"]
-    }
-    req = send('POST', 'api/protocols', message)
-    cipher = req.json().get('cipher')
-    mode = req.json().get('mode')
-    digest = req.json().get('digest')
+    global mode
+    global cipher
+
+    if mode == 'CBC':
+        mode = modes.CBC(iv)
+    elif mode == 'GCM':
+        mode = modes.GCM(iv)
+    elif mode == 'ECB':
+        mode = modes.ECB() 
+
+    if cipher == "ChaCha20":
+        key = derived_key[:32]
+        algorithm = algorithms.ChaCha20(key, iv)
+        _cipher = Cipher(algorithm, mode=None, backend=default_backend())
+    elif cipher == "AES":
+        key = derived_key[:16]
+        algorithm = algorithms.AES(key)
+        _cipher = Cipher(algorithm, mode, backend=default_backend())
+    elif cipher == "3DES":
+        key = derived_key[:8]
+        algorithm = algorithms.TripleDES(key)
+        _cipher = Cipher(algorithm, mode, backend=default_backend())
+
+    decryptor = _cipher.decryptor()
+    text = decryptor.update(cryptogram) + decryptor.finalize()
+
+    if cipher == 'ChaCha20':
+        return text
+
+    unpadder = padding.PKCS7(algorithm.block_size).unpadder()
+    text = unpadder.update(text) + unpadder.finalize()
+
+    return text
+
+def get_dh_keys():
+
+    global derived_key
+    global cipher
+    global mode
+    global digest
+    global parameters
+    global private_key
+
+    print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA PARKOUR")
 
     if not parameters:
         parameters = dh.generate_parameters(generator=2, key_size=1024, backend=default_backend())  
@@ -70,8 +149,8 @@ def main():
             'p': parameter_numbers.p, 
             'g': parameter_numbers.g
         }
-        req = send('POST', 'api/protocols', message)
-        response = req.json()
+        req = send('POST', 'api/key', message)
+        response = receive(req)
 
     if not private_key:
         # generate a secret key
@@ -83,31 +162,66 @@ def main():
             'type': 'KEY_EXCHANGE',
             'public_key': public_key.public_bytes(Encoding.PEM, PublicFormat.SubjectPublicKeyInfo).decode() 
         }
-        req = send('POST', 'api/protocols', message)
+        req = send('POST', 'api/key', message)
 
-    server_public_key = load_pem_public_key(response.get('public_key').encode(), backend=default_backend())
-    shared_secret = private_key.exchange(server_public_key)
+        server_public_key = load_pem_public_key(response.get('public_key').encode(), backend=default_backend())
+        shared_secret = private_key.exchange(server_public_key)
 
-    logger.debug(f'Got shared secret: {shared_secret}')
+        print(f'Got shared secret: {shared_secret}')
 
-    if digest == 'SHA-256':
-        algorithm = hashes.SHA256()
-    elif digest == 'SHA-384':
-        algorithm = hashes.SHA384()
-    elif digest == 'SHA-512':
-        algorithm = hashes.SHA512()
+        if digest == 'SHA-256':
+            algorithm = hashes.SHA256()
+        elif digest == 'SHA-384':
+            algorithm = hashes.SHA384()
+        elif digest == 'SHA-512':
+            algorithm = hashes.SHA512()
 
-    # Derive key
-    derived_key = HKDF(
-        algorithm = algorithm,
-        length = algorithm.digest_size,
-        salt = None,
-        info = b'handshake',
-        backend = default_backend()
-    ).derive(shared_secret)
+        # Derive key
+        derived_key = HKDF(
+            algorithm = algorithm,
+            length = algorithm.digest_size,
+            salt = None,
+            info = b'handshake',
+            backend = default_backend()
+        ).derive(shared_secret)
 
-    print(f'Got derived_key: {derived_key} with length {len(derived_key)}')
-    
+        print(f'Got derived_key: {derived_key} with length {len(derived_key)}')
+
+def main():
+
+    global derived_key
+    global cipher
+    global mode
+    global digest
+    global parameters
+    global private_key
+
+    print("|--------------------------------------|")
+    print("|         SECURE MEDIA CLIENT          |")
+    print("|--------------------------------------|\n")
+
+    # Get a list of media files
+    print("Contacting Server")
+
+    # TODO: Secure the session
+    # req = requests.get(f'{SERVER_URL}/api/protocols')
+    # if req.status_code == 200:
+    #    prots = receive(req)
+
+    message = {
+        'type': 'NEGOTIATE',
+        'mode': ["CBC", "GCM", "ECB"], 
+        'cipher': ["ChaCha20", "AES", "3DES"], 
+        'digest': ["SHA-256", "SHA-384", "SHA-512"]
+    }
+    req = send('POST', 'api/protocols', message)
+    response = receive(req)
+    cipher = response.get('cipher')
+    mode = response.get('mode')
+    digest = response.get('digest')
+
+    get_dh_keys()
+
 
 
     ##############################################################################################
@@ -116,10 +230,11 @@ def main():
 
 
     req = requests.get(f'{SERVER_URL}/api/list')
+    req = send("GET", "api/list")
     if req.status_code == '200':
         print("Got Server List")
 
-    media_list = req.json()
+    media_list = receive(req).get('data')
 
 
     # Present a simple selection menu    
@@ -156,15 +271,23 @@ def main():
     # Get data from server and send it to the ffplay stdin through a pipe
     for chunk in range(media_item['chunks'] + 1):
         req = requests.get(f'{SERVER_URL}/api/download?id={media_item["id"]}&chunk={chunk}')
-        chunk = req.json()
+        chunk = receive(req)
        
         # TODO: Process chunk
+        if chunk.get('type') == 'REGEN_KEY':
+            parameters = None
+            private_key = None
+            get_dh_keys()
+            last_chunk =  chunk['last_chunk']
+            req = requests.get(f'{SERVER_URL}/api/download?id={media_item["id"]}&chunk={last_chunk}')
+            chunk = receive(req)
 
         data = binascii.a2b_base64(chunk['data'].encode('latin'))
         try:
             proc.stdin.write(data)
         except:
             break
+        chunk = 1
 
 if __name__ == '__main__':
     while True:
--- server/server.py
+++ server/server.py
@@ -8,6 +8,7 @@ import base64
 import json
 import os
 import math
+import datetime
 
 from cryptography.hazmat.primitives import hashes, hmac
 from cryptography.hazmat.backends import default_backend
@@ -34,12 +35,15 @@ CATALOG = { '898a08080d1840793122b7e118b27a95d117ebce':
         }
 
 CATALOG_BASE = 'catalog'
+LICENSE_BASE = 'licenses'
 CHUNK_SIZE = 1024 * 4
+KEY_ROTATION_NR = 20
 
 class MediaServer(resource.Resource):
     isLeaf = True
     derived_key = None
     cipher = None
+    count = 0
 
     # Send the list of media files to clients
     def do_list(self, request):
@@ -64,44 +68,53 @@ class MediaServer(resource.Resource):
 
         # Return list to client
         request.responseHeaders.addRawHeader(b"content-type", b"application/json")
-        return json.dumps(media_list, indent=4).encode('latin')
+        message = {
+            'data': media_list
+        }
 
-    # Method used to send data to the client
+        # return json.dumps(media_list, indent=4).encode('latin')
+        return self.send(message)
+
     def send(self, message):
+        """
+        Method used to encode and send a message to the client
+
+        @param: message The message to encode
+        @return The encoded message, with the encryped data, the iv in full and the MAC  
+        """
 
         # Secure connection, after session keys are generated
         if self.derived_key and self.cipher:
-            logger.debug("secure connection")
+            logger.debug(f"send via secure connection")
             cryptogram, iv = self.encrypt(json.dumps(message).encode('latin'))
             
             message = {
                 'type': 'SECURE_MSG',
-                'data': base64.b64encode(cryptogram).decode(),
-                'iv' : base64.b64encode(iv).decode()
+                'data': binascii.b2a_base64(cryptogram).decode(),
+                'iv' : binascii.b2a_base64(iv).decode()
             }
-
-            logger.debug(f'{message}')
-            logger.debug(f'generated cryptogram: {cryptogram}')
-            logger.debug(f'generated iv: {iv}')
 
             mac = self.mac(json.dumps(message).encode('latin'))
 
             message = {
-                'type': "SECURE_MSG",
-                'mac': base64.b64encode(mac).decode(),
-                'iv':  message.get('iv'),
-                'data': message.get('data')
+                'type': "MAC_MSG",
+                'mac': binascii.b2a_base64(mac).decode(),
+                'data':  json.dumps(message),
             }
 
-            logger.debug(f'Send message {message}')
+            self.count += 1
 
-        return json.dumps(message).encode('latin'); 
+        return json.dumps(message).encode('latin')
 
-    # Encrypt a text message based on the ciphers and modes decided on the negotiation
     def encrypt(self, text):
-        iv = None
+        """
+        Encrypt a text messages based on the ciphers and modees decided on the negotiation
 
-        # Select the algorithm and slice the key 
+        @param: text The text in bytes to enctypt
+        @return The cryptogram of the text using the derived key
+        @return The iv generated
+        """
+        iv = None
         if self.cipher == "ChaCha20":
             key = self.derived_key[:32]
             iv = os.urandom(16)
@@ -138,8 +151,14 @@ class MediaServer(resource.Resource):
         cryptogram = encryptor.update(text) + encryptor.finalize()
         return cryptogram, iv
 
-    # Method used to hash the MAC
     def mac(self, message):
+        """
+        Method used to hash the MAC to authenticate a message
+        
+        @param The message as bytes to hash and authenticate.
+        @return The message digest as bytes
+        """
+
         # Slice key based on algorithm
         if self.cipher == "ChaCha20":
             key = self.derived_key[:32]
@@ -161,28 +180,46 @@ class MediaServer(resource.Resource):
         return digest.finalize()
 
     def do_get_protocols(self, request):
-            request.setResponseCode(200)
-            request.responseHeaders.addRawHeader(b"content-type", b"application/json")
-            message = {
-                'type': 'PROTOOCOLS',
-                'data': {'negotiate': 'NEGOTIATE', 'dh_init': 'DH_INIT'}
-            }
-            return self.send(message)
+        """
+        Processes the GET request to the protocols endpoint
+
+        @oaram: request The request tht came from the client in bytes
+        @return The response, in bytes, to the client
+        """
+
+        request.setResponseCode(200)
+        request.responseHeaders.addRawHeader(b"content-type", b"application/json")
+        message = {
+            'type': 'PROTOOCOLS',
+            'data': {'negotiate': 'NEGOTIATE', 'dh_init': 'DH_INIT'}
+        }
+        return self.send(message)
 
     def do_post_protocols(self, request):
+        """
+        Processes a POST request to the protocols endpoint based on the type of message
+
+        @oaram: request The request that came from the client in bytes
+        @return The encrypted response sent to the client
+        """
+
+        # TODO: authenticate the request
+
         if (request.args.get(b'type')):
             msg_type = request.args.get(b'type')[0]
         else:
             msg_type = None
 
-        if msg_type == b'NEGOTIATE':            
+        if msg_type == b'NEGOTIATE':
+
+            self.derived_key = None
             # choose the alortithm and mode
             if self.choose_suite(request.args.get(b'cipher'), request.args.get(b'mode'), request.args.get(b'digest')):
                 request.setResponseCode(200)
                 request.responseHeaders.addRawHeader(b"content-type", b"application/json")
                 message = {
                     'type': 'NEGOTIATE',
-                    '"mode': self.mode,
+                    'mode': self.mode,
                     'cipher': self.cipher, 
                     'digest': self.digest
                 }
@@ -198,60 +235,17 @@ class MediaServer(resource.Resource):
                 }
                 return self.send(message)
 
-        elif msg_type == b'DH_INIT':
-            p = int(request.args.get(b'p')[0].decode())
-            g = int(request.args.get(b'g')[0].decode())
-
-            self.parameters = dh.DHParameterNumbers(p, g).parameters(default_backend())
-
-            # generate a secret key
-            self.private_key = self.parameters.generate_private_key()
-
-            # generate the public key based on the private one -> g^(private_key) mod p 
-            self.public_key = self.private_key.public_key()
-
-            request.setResponseCode(200)
-            request.responseHeaders.addRawHeader(b"content-type", b"application/json")
-            message = {
-                'type': 'DH_INIT',
-                'public_key': self.public_key.public_bytes(Encoding.PEM, PublicFormat.SubjectPublicKeyInfo).decode() 
-            }
-            return self.send(message)
-
-        elif msg_type == b'KEY_EXCHANGE':
-            logger.debug("Key exchange: received public key from client")
-            # p, g, a e B = public_key
-            #
-            client_public_key = load_pem_public_key(request.args.get(b'public_key')[0], backend=default_backend())
-            shared_secret = self.private_key.exchange(client_public_key)
-
-            logger.debug(f'Got shared secret: {shared_secret}')
-
-            if self.digest == 'SHA-256':
-                algorithm = hashes.SHA256()
-            elif self.digest == 'SHA-384':
-                algorithm = hashes.SHA384()
-            elif self.digest == 'SHA-512':
-                algorithm = hashes.SHA512()
-
-            # Derive key
-            self.derived_key = HKDF(
-                algorithm = algorithm,
-                length = algorithm.digest_size,
-                salt = None,
-                info = b'handshake',
-                backend = default_backend()
-            ).derive(shared_secret)
-
-            logger.debug(f'Got derived_key: {self.derived_key}')
-            request.responseHeaders.addRawHeader(b"content-type", b"application/json")
-            message = {
-                'type': 'OK',
-                'data': {}
-            }
-            return self.send(message)
-
     def choose_suite(self, ciphers, modes, digests):
+        """
+        Algorithm used to choose and set the MediaServer's cipher, mode and digest based on
+        which combination of the client's avalable one is the most secure
+
+        @param: ciphers A list of the client's available cipher algorithms
+        @param: modes A list of the client's available modes to cipher
+        @param: diigests A list of the client's available digest algorithms 
+        @return True if a suite has been successefully chosen, False otherwise
+        """
+
         # choose cipher
         if b'ChaCha20' in ciphers:
             self.cipher = 'ChaCha20'
@@ -287,6 +281,142 @@ class MediaServer(resource.Resource):
 
         return True
 
+    def do_dh_exchange(self, request):
+        """
+        Processes a POST request to the api/key endpoint, which triggeres the Diffie-Hellman's 
+        key generation process that generates the p and g parameters, generates a private key 
+        and a shared secret, as well as deriving the latter in order to create ephemeral keys
+        for each session.
+        This process is then repeated when the number received messages from the client surpasses 20,
+        to assure key rotation and therefore a extra level of security.  
+
+        @param: request Request that come from the client in bytes
+        @return The encrypted response sent to the client
+        """
+
+        msg_type = None
+        if (request.args.get(b'type')):
+            msg_type = request.args.get(b'type')[0]
+
+        if msg_type == b'DH_INIT':
+            p = int(request.args.get(b'p')[0].decode())
+            g = int(request.args.get(b'g')[0].decode())
+
+            self.parameters = dh.DHParameterNumbers(p, g).parameters(default_backend())
+
+            # generate a secret key
+            self.private_key = self.parameters.generate_private_key()
+
+            # generate the public key based on the private one -> g^(private_key) mod p 
+            self.public_key = self.private_key.public_key()
+
+            request.setResponseCode(200)
+            request.responseHeaders.addRawHeader(b"content-type", b"application/json")
+            message = {
+                'type': 'DH_INIT',
+                'public_key': self.public_key.public_bytes(Encoding.PEM, PublicFormat.SubjectPublicKeyInfo).decode() 
+            }
+            return self.send(message)
+
+        elif msg_type == b'KEY_EXCHANGE':
+
+            client_public_key = load_pem_public_key(request.args.get(b'public_key')[0], backend=default_backend())
+            shared_secret = self.private_key.exchange(client_public_key)
+
+            logger.debug(f'Got shared secret: {shared_secret}')
+
+            if self.digest == 'SHA-256':
+                algorithm = hashes.SHA256()
+            elif self.digest == 'SHA-384':
+                algorithm = hashes.SHA384()
+            elif self.digest == 'SHA-512':
+                algorithm = hashes.SHA512()
+
+            # Derive key
+            self.derived_key = HKDF(
+                algorithm = algorithm,
+                length = algorithm.digest_size,
+                salt = None,
+                info = b'handshake',
+                backend = default_backend()
+            ).derive(shared_secret)
+
+            logger.debug(f'Got derived_key: {self.derived_key}')
+            request.responseHeaders.addRawHeader(b"content-type", b"application/json")
+            message = {
+                'type': 'OK',
+                'data': {}
+            }
+
+            return self.send(message)
+
+    def do_regen_key(self, request):
+        """
+        Method called when the number of messages from the client reaches a certain number,
+        in order to assure key rotation, the triggers the Diffie-Hellman's process to generate
+        new ephemeral key, generating a new set of private and public keys, both on the client
+        and the server's side.
+
+        @param: request Request that comes from the client in bytes
+        @return The encrypted response sent to the client    
+        """
+
+        self.parameters = None
+        self.private_key = None
+        self.derived_key = None
+        last_chunk = request.args.get(b'chunk', [b'0'])[0].decode()
+        message = {
+            'type': 'REGEN_KEY',
+            'last_chunk': last_chunk
+        }
+        return self.send(message)
+
+    def check_license(self, media_id):
+        """
+        Check if a client has a viewing license for a speciffic media by reading the respective
+        media's file license and checking if the client is listed and if the licence is valid.
+        While it searches for the client's license, this method also deletes expired licenses in
+        order to improve performance.
+
+        @param: media_id The id of media the client wants to play
+        @retur True if the license is valid, False otherwise
+        """
+
+        with open(os.path.join(LICENSE_BASE, media_id), 'rb+') as f:
+            # f.write(binascii.b2a_base64("0001\t1978-10-2 12:12:12.000\t10\n".encode('latin')))
+            # f.write(binascii.b2a_base64("0001\t2020-12-30 12:00:00.000\t10000\n".encode('latin')))
+            # f.seek(0)
+
+            valid = None
+            d = f.readlines()
+            f.seek(0)
+
+            for line in d:
+                line = binascii.a2b_base64(line)
+                client_id = b'0001'
+                _line = line.strip().split(b'\t')
+
+                if client_id == _line[0]:
+                    if datetime.datetime.strptime(_line[1].decode(), '%Y-%m-%d %H:%M:%S.%f') + datetime.timedelta(seconds= int(_line[2].decode())) < datetime.datetime.now():
+                        if valid != True:
+                            logger.debug('This viewing license expired')
+                            valid = False
+                    else:
+                        # There is a valid and not expired license in the client's name
+                        f.write(binascii.b2a_base64(line))
+                        valid = True
+                
+                else:
+                    f.write(binascii.b2a_base64(line))
+                    if valid != True:
+                        logger.debug('Client has no rights to view this content')
+                        valid = False
+
+            f.truncate()
+            f.close()
+        
+        return valid
+
     # Send a media chunk to the client
     def do_download(self, request):
         logger.debug(f'Download: args: {request.args}')
@@ -311,6 +441,12 @@ class MediaServer(resource.Resource):
         
         # Get the media item
         media_item = CATALOG[media_id]
+
+        # Check licenciong
+        if not self.check_license(media_id):
+            request.setResponseCode(403)
+            request.responseHeaders.addRawHeader(b"content-type", b"application/json")
+            return json.dumps({'error': 'No access rights to the content'}).encode('latin')
 
         # Check if a chunk is valid
         chunk_id = request.args.get(b'chunk', [b'0'])[0]
@@ -337,13 +473,12 @@ class MediaServer(resource.Resource):
             data = f.read(CHUNK_SIZE)
 
             request.responseHeaders.addRawHeader(b"content-type", b"application/json")
-            return json.dumps(
-                    {
+            message = {
                         'media_id': media_id, 
                         'chunk': chunk_id, 
                         'data': binascii.b2a_base64(data).decode('latin').strip()
-                    },indent=4
-                ).encode('latin')
+                    }
+            return self.send(message)
 
         # File was not open?
         request.responseHeaders.addRawHeader(b"content-type", b"application/json")
@@ -352,12 +487,16 @@ class MediaServer(resource.Resource):
     # Handle a GET request
     def render_GET(self, request):
         logger.debug(f'Received request for {request.uri}')
+        
+        if self.count == KEY_ROTATION_NR:
+            self.count = 0
+            logger.debug("Regening keys for key rotation..")
+            return self.do_regen_key(request)
+
 
         try:
             if request.path == b'/api/protocols':
                 return self.do_get_protocols(request)
-            #elif request.uri == 'api/key':
-            #...
             #elif request.uri == 'api/auth':
 
             elif request.path == b'/api/list':
@@ -380,6 +519,8 @@ class MediaServer(resource.Resource):
         logger.debug(f'Received POST for {request.uri}')
         if request.path == b'/api/protocols':
             return self.do_post_protocols(request)
+        elif request.uri == b'/api/key':
+            return self.do_dh_exchange(request)
         request.setResponseCode(501)
         return b''
 
